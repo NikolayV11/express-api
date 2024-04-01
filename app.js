57eab5bee2e2c32fb738bf189118136d77dbcc39
Николай <wasilev-n@mail.ru>
2024-04-01 17:24:40 +0500

    импорт cors  — это пакет node.js для предоставления промежуточного программного обеспечения Connect / Express , которое можно использовать для включения CORS с различными параметрами.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require("path");
 const fs = require("fs");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
